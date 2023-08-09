b3f9012ebfff9031bfe9b3b0199e5cb271717eca
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-08-09 13:24:41 +0800

    Tests + function

--- src/__tests__/orderByProps.js
+++ src/__tests__/orderByProps.js
@@ -1,5 +1,149 @@
 import orderByProps from '../orderByProps';
 
-test('Test', () => {
-  expect(orderByProps({})).toEqual({});
+test.each([
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['name', 'level'],
+    [
+      { key: 'name', value: 'мечник' },
+      { key: 'level', value: 2 },
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+    ],
+  ],
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['health', 'defence'],
+    [
+      { key: 'health', value: 10 },
+      { key: 'defence', value: 40 },
+      { key: 'attack', value: 80 },
+      { key: 'level', value: 2 },
+      { key: 'name', value: 'мечник' },
+    ],
+  ],
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['attack'],
+    [
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'level', value: 2 },
+      { key: 'name', value: 'мечник' },
+    ],
+  ],
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['level', 'attack', 'health'],
+    [
+      { key: 'level', value: 2 },
+      { key: 'attack', value: 80 },
+      { key: 'health', value: 10 },
+      { key: 'defence', value: 40 },
+      { key: 'name', value: 'мечник' },
+    ],
+  ],
+
+])('Test', (obj, props, result) => {
+  expect(orderByProps(obj, props)).toEqual(result);
+});
+
+test.each([
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['name', 'level'],
+    [
+      { key: 'name', value: 'мечник' },
+      { key: 'level', value: 2 },
+      { key: 'defence', value: 40 },
+      { key: 'attack', value: 80 },
+      { key: 'health', value: 10 },
+    ],
+  ],
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['health', 'defence'],
+    [
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'attack', value: 80 },
+      { key: 'level', value: 2 },
+      { key: 'name', value: 'мечник' },
+    ],
+  ],
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['attack'],
+    [
+      { key: 'attack', value: 80 },
+      { key: 'defence', value: 40 },
+      { key: 'health', value: 10 },
+      { key: 'name', value: 'мечник' },
+      { key: 'level', value: 2 },
+    ],
+  ],
+  [
+    {
+      name: 'мечник',
+      health: 10,
+      level: 2,
+      attack: 80,
+      defence: 40,
+    },
+    ['level', 'attack', 'health'],
+    [
+      { key: 'attack', value: 80 },
+      { key: 'health', value: 10 },
+      { key: 'level', value: 2 },
+      { key: 'defence', value: 40 },
+      { key: 'name', value: 'мечник' },
+    ],
+  ],
+
+])('Test with wrong orders', (obj, props, result) => {
+  expect(orderByProps(obj, props)).not.toEqual(result);
 });
--- src/orderByProps.js
+++ src/orderByProps.js
@@ -1,3 +1,15 @@
-export default function orderByProps(obj) {
-  return obj;
+export default function orderByProps(obj, props) {
+  const result = [];
+  const keys = Object.keys(obj).sort();
+  props.forEach((prop) => {
+    if (prop in obj) {
+      result.push({ key: prop, value: obj[prop] });
+    }
+  });
+  keys.forEach((key) => {
+    if (!props.includes(key)) {
+      result.push({ key, value: obj[key] });
+    }
+  });
+  return result;
 }
